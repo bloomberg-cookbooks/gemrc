db3ba653db6f0680b739b79232c9d5bda1c87ce6
Matt Kulka <mkulka@parchment.com>
2019-07-18 13:16:47 -0700

    add sensitive property to resource and recipe
    gemrc can contain sensitive information like username and password in sources. adding an
    attribute for `sensitive` to allow for hiding file contents in logs during update.

--- resources/file.rb
+++ resources/file.rb
@@ -14,6 +14,7 @@ property :group, String, default: lazy { node['root_group'] }
 property :mode, String, default: '0644'
 
 property :reload, [true, false], default: false
+property :sensitive, [true, false], default: false
 property :options, Hash, default: { 'gem' => '--no-ri --no-rdoc' }
 
 def to_yaml
@@ -37,6 +38,7 @@ action :create do
     owner new_resource.owner
     group new_resource.group
     mode new_resource.mode
+    sensitive new_resource.sensitive
   end
 
   if new_resource.reload && r.updated_by_last_action?
